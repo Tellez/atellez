d1231dce4260dd43159e7b0870d95252739959c5
atellez <atellez@106-22.info2>
2012-05-21 13:23:18 +0200

    Se añade "método_rama1_clase1" a la "clase1".

--- src/clase1.java
+++ src/clase1.java
@@ -7,4 +7,8 @@ public class clase1 {
 	public void metodo1_clase1(){
 		System.out.println("Primer commit realizado a la clase 1");
 	}
+	
+	public void metodo_rama1_clase1(){
+		System.out.println("Segundo commit realizado a la clase 1");
+	}
 }
